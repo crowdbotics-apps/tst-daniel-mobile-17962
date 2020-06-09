0454bf06e1e64054385df58f0e1192df7e4e2c34
Crowdbotics <team@crowdbotics.com>
2020-06-09 15:10:52 +0000

    Update blank.js JSX

--- src/features/BlankScreen64683/screens/blank.js
+++ src/features/BlankScreen64683/screens/blank.js
@@ -16,7 +16,14 @@ export class _Blank extends React.Component {
     title: "Blank".toUpperCase()
   }
 
-  state = { Input_7: "" }
+  state = {
+    Input_7: "",
+    CheckBox_12: true,
+    Toggle_14: true,
+    Input_21: "",
+    CheckBox_31: true,
+    Radio_27: true
+  }
 
   render = () => (
     <View
@@ -103,6 +110,69 @@ export class _Blank extends React.Component {
               <Text style={{ overflow: "visible", borderStyle: "solid" }}>
                 Text
               </Text>
+              <Slider
+                value="50"
+                minimalValue="0"
+                maximumValue="100"
+                step="1"
+                disabled={false}
+                style={{ overflow: "visible", borderStyle: "solid" }}
+              />
+              <Image
+                resizeMethod="cover"
+                style={{ overflow: "visible", borderStyle: "solid" }}
+              />
+              <CheckBox
+                text="Checkbox"
+                disabled={false}
+                style={{ overflow: "visible", borderStyle: "solid" }}
+                checked={this.state.CheckBox_12}
+                onChange={nextChecked =>
+                  this.setState({ CheckBox_12: nextChecked })
+                }
+              />
+              <View
+                style={{
+                  overflow: "visible",
+                  flexDirection: "column",
+                  flexWrap: "wrap",
+                  justifyContent: "flex-start",
+                  alignItems: "stretch",
+                  alignContent: "stretch",
+                  borderStyle: "solid",
+                  backgroundSize: "auto"
+                }}
+              >
+                <View
+                  style={{
+                    overflow: "visible",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "flex-start",
+                    alignItems: "stretch",
+                    alignContent: "stretch",
+                    borderStyle: "solid",
+                    backgroundSize: "auto"
+                  }}
+                >
+                  <Image
+                    resizeMethod="cover"
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                  />
+                </View>
+                <View
+                  style={{
+                    overflow: "visible",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "flex-start",
+                    alignItems: "stretch",
+                    alignContent: "stretch",
+                    borderStyle: "solid",
+                    backgroundSize: "auto"
+                  }}
+                />
+              </View>
             </View>
             <View
               style={{
@@ -153,6 +223,59 @@ export class _Blank extends React.Component {
                   >
                     Button
                   </Button>
+                  <Toggle
+                    activeColor=""
+                    inactiveColor=""
+                    disabled={false}
+                    text="switch ON/OFF"
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                    checked={this.state.Toggle_14}
+                    onChange={nextChecked =>
+                      this.setState({ Toggle_14: nextChecked })
+                    }
+                  />
+                  <Input
+                    number
+                    placeholder="Text input placeholder"
+                    editable={true}
+                    value=""
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                  />
+                  <Textarea
+                    placeholder="Text input placeholder"
+                    editable={true}
+                    value=""
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                  />
+                  <Input
+                    placeholder="Text input placeholder"
+                    editable={true}
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                    value={this.state.Input_21}
+                    onChangeText={nextValue =>
+                      this.setState({ Input_21: nextValue })
+                    }
+                  />
+                  <Text style={{ overflow: "visible", borderStyle: "solid" }}>
+                    Text
+                  </Text>
+                  <Icon
+                    iconFont="Eva Design Icons"
+                    icon="github"
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                  />
+                  <CheckBox
+                    text="Checkbox"
+                    disabled={false}
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                    checked={this.state.CheckBox_31}
+                    onChange={nextChecked =>
+                      this.setState({ CheckBox_31: nextChecked })
+                    }
+                  />
+                  <Text style={{ overflow: "visible", borderStyle: "solid" }}>
+                    Text
+                  </Text>
                 </View>
                 <View
                   style={{
@@ -172,6 +295,15 @@ export class _Blank extends React.Component {
                   <Text style={{ overflow: "visible", borderStyle: "solid" }}>
                     Text
                   </Text>
+                  <Radio
+                    text="Radio button"
+                    disabled={false}
+                    style={{ overflow: "visible", borderStyle: "solid" }}
+                    checked={this.state.Radio_27}
+                    onChange={nextChecked =>
+                      this.setState({ Radio_27: nextChecked })
+                    }
+                  />
                 </View>
               </View>
             </View>
